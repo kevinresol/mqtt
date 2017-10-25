310dace37b674f244821a24c04ed892f415d0de4
Kevin Leung <kevinresol@gmail.com>
2017-10-26 10:49:39 +1100

    Test

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -2,11 +2,14 @@ package ;
 
 import tink.testrunner.*;
 import tink.unit.*;
+import haxe.io.Bytes;
+import asys.io.*;
 import mqtt.clients.*;
 
 using tink.CoreApi;
 
 @:asserts
+@:timeout(8000)
 class RunTests {
 
   static function main() {
@@ -17,30 +20,114 @@ class RunTests {
   
   function new() {}
   
-  public function echo() {
-    ReactNativePahoClient;
-    // var client = new KeepAliveClient('mqtt://test.mosquitto.org', NodeClient.new);
-    var client = new NodeClient('mqtt://test.mosquitto.org');
-    client.connect()
-      .handle(function(o) switch o {
-        case Success(_):
-          var count = 0;
-          var topic = 'haxe-mqtt-' + Date.now().getTime();
-          client.message.handle(function(m) {
-            asserts.assert(m.a == topic);
-            asserts.assert(m.b == 'after');
-            if(++count == 3) asserts.done();
+  function killAll() {
+    return Future.async(function(cb) {
+      function check()
+        return Promise.ofJsPromise(js.Lib.require('find-process')('port', 1883))
+          .next(function(list:Array<{pid:String}>) {
+              for(p in list) Sys.command('kill', [p.pid]);
+              return list.length;
           });
-          client.publish(topic, 'before');
-          client.publish(topic, 'before');
-          client.publish(topic, 'before');
-          client.subscribe(topic);
-          client.publish(topic, 'after');
-          client.publish(topic, 'after');
-          client.publish(topic, 'after');
+      
+      function recheck()
+        check().handle(function(o) switch o {
+          case Success(0): cb(Success(Noise));
+          case Success(_): recheck();
+          case Failure(e): cb(Failure(e));
+        });
+        
+      recheck();
+    });
+  }
+  
+  function runBroker() {
+    return killAll()
+      .next(function(_) {
+        var proc = new Process('npm', ['run', 'mosca']);
+        proc.stderr.all().handle(function(o) switch o.sure().toString() {
+          case '': 
+          case v: trace(v);
+        });
+        return Future.async(function(cb) {
+          function check() {
+            Promise.ofJsPromise(js.Lib.require('find-process')('port', 1883))
+              .handle(function(o) switch o {
+                case Success(list) if(list.length == 0): haxe.Timer.delay(check, 100);
+                case Success(_): cb(Success(proc));
+                case Failure(e): cb(Failure(e));
+              });
+          }
+          check();
+        });
+      });
+  }
+  
+  public function echo() {
+    runBroker()
+      .handle(function(o) switch o {
+        case Success(broker):
+          var client = new NodeClient('mqtt://localhost');
+          client.connect()
+            .handle(function(o) switch o {
+              case Success(_):
+                var count = 0;
+                var topic = 'haxe-mqtt-' + Date.now().getTime();
+                client.message.handle(function(m) {
+                  asserts.assert(m.a == topic);
+                  asserts.assert(m.b == 'after');
+                  if(++count == 3) {
+                    asserts.done();
+                  }
+                });
+                client.publish(topic, 'before');
+                client.publish(topic, 'before');
+                client.publish(topic, 'before');
+                client.subscribe(topic);
+                client.publish(topic, 'after');
+                client.publish(topic, 'after');
+                client.publish(topic, 'after');
+              case Failure(e):
+                asserts.fail(e);
+            });
         case Failure(e):
           asserts.fail(e);
       });
-    return asserts;
+      
+      return asserts;
+  }
+  
+  public function retry() {
+    runBroker()
+      .handle(function(o) switch o {
+        case Success(broker):
+          var client = new KeepAliveClient('mqtt://test.mosquitto.org', NodeClient.new);
+          client.connect()
+            .handle(function(o) switch o {
+              case Success(_):
+                var count = 0;
+                var topic = 'haxe-mqtt-' + Date.now().getTime();
+                client.message.handle(function(m) {
+                  asserts.assert(m.a == topic);
+                  asserts.assert(m.b == 'after');
+                  if(++count == 3) {
+                    asserts.done();
+                  }
+                });
+                client.publish(topic, 'before');
+                client.publish(topic, 'before');
+                client.publish(topic, 'before');
+                client.subscribe(topic);
+                client.publish(topic, 'after');
+                runBroker().handle(function(_) {
+                  client.publish(topic, 'after');
+                  client.publish(topic, 'after');
+                });
+              case Failure(e):
+                asserts.fail(e);
+            });
+        case Failure(e):
+          asserts.fail(e);
+      });
+      return asserts;
   }
 }
